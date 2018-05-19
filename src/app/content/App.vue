e35ecb319ffe1f5571fc37c12b07a6ca7529cf45
Pavel Koch <pavel@joinpouch.com>
2018-05-19 19:50:02 +0100

    added vue instance to content script

---
+++ src/app/content/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <!--  -->
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class App extends Vue {
+  //
+}
+</script>
